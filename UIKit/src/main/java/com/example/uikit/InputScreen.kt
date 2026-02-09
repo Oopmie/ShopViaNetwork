27e7d5e6fd99d46b21967b0bed0eec51c10d2da1
oopmie <oopmie@mail.ru>
2026-02-09 19:25:30 +0300

    input

--- UIKit/src/main/java/com/example/uikit/InputScreen.kt
+++ UIKit/src/main/java/com/example/uikit/InputScreen.kt
@@ -1,7 +1,5 @@
 package com.example.uikit
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,60 +8,52 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import com.example.uikit.ui.theme.Error
+import com.example.uikit.ui.theme.InputBg
 
 //вводы текста для регистрации
 @Composable
 fun AppTextField(
     value: String,
-    onValueChange: (String) -> Unit,
+    onValueChange: (String)-> Unit,
     placeholder: String,
-    isError: Boolean = false, // Передаем true, если кнопка нажата, а текста нет
-    errorText: String = "Введите ваш текст",
+    isError: Boolean = false,
+    errorText: String,
     modifier: Modifier = Modifier
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
-    val isFocused by interactionSource.collectIsFocusedAsState()
-
-    // Логика цвета границ:
-    // 1. Если ошибка -> Красный
-    // 2. Если нажат (фокус) -> Синий
-    // 3. Если есть текст -> Темно-серый
-    // 4. По умолчанию -> Светло-серый
+    val isFocused by mutableStateOf(false)
     val borderColor = when {
-        isError -> Color.Red
-        isFocused -> Color(0xFF007AFF) // Твой синий (Accent)
-        value.isNotEmpty() -> Color.DarkGray
+        isError -> Error
+        isFocused -> Color(0xFF007AFF)
+        value.isNotEmpty()-> Color(0xFFB8C1CC)
         else -> Color.LightGray
     }
 
     Column (modifier = modifier.fillMaxWidth()) {
         OutlinedTextField(
-            value = value,
-            onValueChange = onValueChange,
+            value=value,
+            onValueChange=onValueChange,
             modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = placeholder, color = Color.Gray) },
+            placeholder = {Text(text = placeholder)},
             isError = isError,
-            interactionSource = interactionSource,
-            shape = RoundedCornerShape(12.dp),
-            // Настройка цветов через Colors
+            shape = RoundedCornerShape(10.dp),
             colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = Color(0xFF007AFF),
-                unfocusedBorderColor = borderColor, // Используем нашу логику
-                errorBorderColor = Color.Red,
-                focusedContainerColor = Color.White,
-                unfocusedContainerColor = Color.White,
+                unfocusedBorderColor = borderColor,
+                errorBorderColor = Error,
+                errorContainerColor = Color(0xFFE8A3A3FF),
+                focusedContainerColor = InputBg,
+                unfocusedContainerColor = InputBg,
             ),
             singleLine = true
         )
-
-        // Показ текста ошибки под полем
         if (isError) {
             Text(
                 text = errorText,
